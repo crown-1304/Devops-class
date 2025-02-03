997178d6a0a8dc3a40a60f841fff445dd93abc52
Crown.G.K. Srinivas <gksrinivas675@gmail.com>
2025-02-03 18:57:49 +0530

    Version-1

---
+++ train.py
@@ -0,0 +1,15 @@
+sudo -i - root user
+apt/yum update -update
+apt install net-tools
+apt install vim
+apt install apache2 / yum install httpd
+systemctl status apache2
+cd /var/www/html
+mv index.html _______
+vi index.html
+--insert--
+(add your content)
+TO SAVE
+press - esc - shift+: wq- to save and exit / q! - exit without saving
+
+Hii Hello Vanakkam
